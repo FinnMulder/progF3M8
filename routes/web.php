a59f498cc60936ebcd02ab89de8e82312b31d609
Finn Mulder <32256@ma-web.nl>
2022-07-01 16:31:16 +0200

    ik heb tot en met 5.3 af

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AboutController;
+use App\Http\Controllers\BlogAdminController;
 use App\Http\Controllers\Blogcontroller;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\Homecontroller;
@@ -21,12 +22,22 @@ Route::get('/', function (){
 });
 
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+
 
 Route::get('/blog', [\App\Http\Controllers\Blogcontroller::class, 'index'] )->name('blog.index' );
 Route::get('/over-mij', [AboutController::class, 'aboutMe'])->name('about.me');
 Route::get('/contact', [\App\Http\Controllers\ContactController::class, 'contactForm'])->name('contact.form');
+
+
+
+route::prefix('/dashboard')->middleware(['auth'])->group(function(){
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+   // Route::resources([
+      //  'articles' => BlogAdminController::Controller::class,
+ // ]);
+});
+
 
 require __DIR__.'/auth.php';
